fa6dcec1701e19004da3ec7e307be6c5dadb2b70
Tuan Nguyen <anhtuank7c@hotmail.com>
2022-08-15 08:29:13 +0700

    initial tabs component ie: account, coupons, feed, home and shopping cart, also initial account store to saving cart items

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,4 +1,5 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { AccountModel } from "../account/account"
 import { CharacterStoreModel } from "../character-store/character-store"
 
 /**
@@ -7,6 +8,7 @@ import { CharacterStoreModel } from "../character-store/character-store"
 // prettier-ignore
 export const RootStoreModel = types.model("RootStore").props({
   characterStore: types.optional(CharacterStoreModel, {} as any),
+  accountStore: types.optional(AccountModel, {} as any),
 })
 
 /**
--- app/screens/index.ts
+++ app/screens/index.ts
@@ -4,3 +4,8 @@ export * from "./demo/demo-list-screen"
 export * from "./error/error-boundary"
 // export other screens here
 export * from "./intro/intro-screen"
+export * from "./shopping-cart/shopping-cart-screen"
+export * from "./home/home-screen"
+export * from "./feed/feed-screen"
+export * from "./coupons/coupons-screen"
+export * from "./account/account-screen"
