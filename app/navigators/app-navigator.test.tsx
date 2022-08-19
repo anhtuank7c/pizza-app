f0cd275b8983de4a57b3e74e761725a71c56c952
Tuan Nguyen <anhtuank7c@hotmail.com>
2022-08-19 17:13:37 +0700

    Added app-navigator test, snapshot and modify intro-screen snapshot

---
+++ app/navigators/app-navigator.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react-native';
+import { AppNavigator } from './app-navigator';
+
+describe('tabbar-navigator', () => {
+  test('render correctly', () => {
+    render(<AppNavigator />)
+    expect(screen.toJSON()).toMatchSnapshot()
+    expect(screen.findAllByText('Home')).toBeTruthy()
+    expect(screen.findAllByText('Coupons')).toBeTruthy()
+    expect(screen.findAllByText('Cart')).toBeTruthy()
+    expect(screen.findAllByText('Profile')).toBeTruthy()
+  })
+})
