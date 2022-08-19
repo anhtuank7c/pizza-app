d629a41bc5fc2e027cc71cdca98834c68c4e18c1
Tuan Nguyen <anhtuank7c@hotmail.com>
2022-08-19 17:22:54 +0700

    Added jest test for switch

---
+++ app/components/switch/switch.spec.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react-native'
+import { Switch } from './switch'
+
+test('match snapshot', () => {
+  render(<Switch />)
+  expect(screen).toMatchSnapshot()
+})
